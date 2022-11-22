ceb68dffdc2f2aa0262a399225d194269a430de6
daniil <holodnayazvezda@yandex.ru>
2022-11-22 19:45:27 +0300

    third update

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import sys
 
 from PyQt5 import uic  # Импортируем uic
-from PyQt5.QtWidgets import QApplication, QDialog, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QPainter, QColor
-from random import randint, random
+from random import randint
 
 
 class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.qp = QPainter()
-        uic.loadUi('UI.ui', self)  # Загружаем дизайн
-        self.button.clicked.connect(self.run)
+        self.initUi()
         self.now_drowing = False
+
+    def initUi(self):
+        self.setGeometry(300, 300, 900, 600)
+        self.setWindowTitle("Окружности и git")
+        self.button = QPushButton('Кружочки разного цвета!', self)
+        self.button.resize(300, 30)
+        self.button.move(300, 500)
+        self.button.clicked.connect(self.run)
 
     def run(self):
         self.now_drowing = True
@@ -29,9 +36,8 @@ class MyWidget(QWidget):
         self.qp.end()
 
     def draw(self):
-        self.qp.setBrush(QColor(255, 255, 0))
-        self.qp.drawEllipse(randint(0, 300), randint(0, 300), randint(0, 300), randint(0, 300))
-
+        self.qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+        self.qp.drawEllipse(randint(0, 600), randint(0, 600), randint(0, 300), randint(0, 300))
 
 
 if __name__ == '__main__':
